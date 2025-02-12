19c5d2287d717ea50f5f1f5249e86b0f91854a3c
d-alshehri <10674@holbertonstudents.com>
2025-02-12 09:30:17 +0000

    Added script C file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main(void)
+11;rgb:2b2b/2b2b/2b2bint main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
