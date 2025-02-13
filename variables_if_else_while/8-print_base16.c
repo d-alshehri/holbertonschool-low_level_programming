26e8f7d08153ff7e13ac53c1707d9c96678af9c6
d-alshehri <10674@holbertonstudents.com>
2025-02-13 14:05:13 +0000

    T

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: Prints all numbers of base 16 in lowercase
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+putchar(n);
+for (n = 'a'; n <= 'f'; n++)
+putchar(n);
+putchar('\n');
+return (0);
+}
