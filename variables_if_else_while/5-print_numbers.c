172a081eba92eb794fb298d3ac763c2d6d5f0ab8
d-alshehri <10674@holbertonstudents.com>
2025-02-13 13:58:32 +0000

    T

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -4,15 +4,13 @@
 * Description: Prints all single digit numbers of base 10 starting from 0
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
 char n;
-
-for (n = '0' ; n <= '9' ; n++)
+for (n = '0'; n <= '9'; n++)
 {
 putchar(n);
 }
-putchar('\n'); 
+putchar('\n');
 return (0);
 }
