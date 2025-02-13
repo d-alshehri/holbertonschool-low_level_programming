a4d069ef4920e6eee6bd4a06f1c6fe7c1d49e269
d-alshehri <10674@holbertonstudents.com>
2025-02-13 14:34:41 +0000

    T

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -8,7 +8,7 @@ void print_alphabet(void)
 char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
 int i;
 _putchar(alphabet[0]);
- for (i = 1; alphabet[i] != '\0'; i++)
+for (i = 1; alphabet[i] != '\0'; i++)
 {
 _putchar(alphabet[i]);
 }
