f67689dcd04c030171727dbfab755a77574d75ff
d-alshehri <10674@holbertonstudents.com>
2025-02-26 08:06:35 +0000

    T

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,28 +8,28 @@
  */
 char *cap_string(char *str)
 {
-    int i = 0;
-    char separators[] = " \t\n,;.!?\"(){}";
-    int j;
+int i = 0;
+char separators[] = " \t\n,;.!?\"(){}";
+int j;
 
-    while (str[i])
-    {
-        if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
-        {
-            str[i] -= 32;
-        }
-        else
-        {
-            for (j = 0; separators[j] != '\0'; j++)
-            {
-                if (str[i - 1] == separators[j] && str[i] >= 'a' && str[i] <= 'z')
-                {
-                    str[i] -= 32;
-                    break;
-                }
-            }
-        }
-        i++;
-    }
-    return str;
+while (str[i])
+{
+if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] -= 32;
+}
+else
+{
+for (j = 0; separators[j] != '\0'; j++)
+{
+if (str[i - 1] == separators[j] && str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] -= 32;
+break;
+}
+}
+}
+i++;
+}
+{return str; }
 }
