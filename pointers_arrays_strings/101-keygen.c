6227567decbe3591999772769ab200d2d2ac085b
d-alshehri <10674@holbertonstudents.com>
2025-02-27 13:06:27 +0000

    T

---
+++ pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 6
+
+/**
+ * main - generates a random valid password
+ * 
+ * Return: Always 0.
+ */
+int main(void)
+{
+char password[PASSWORD_LENGTH + 1];
+int i;
+
+srand(time(NULL));
+
+for (i = 0; i < PASSWORD_LENGTH; i++)
+{
+password[i] = (rand() % 94) + 33;
+}
+
+password[PASSWORD_LENGTH] = '\0';
+
+printf("%s\n", password);
+
+{return 0; }
+}
