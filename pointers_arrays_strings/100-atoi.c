32f6c4247d90986c62a2332a488e49f20da46020
d-alshehri <10674@holbertonstudents.com>
2025-02-27 12:56:03 +0000

    T

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,20 +7,24 @@
  */
 int _atoi(char *s)
 {
-int i = 0, sign = 1, num = 0;
-while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
-s[i] == '\v' || s[i] == '\f' || s[i] == '\r')
-i++;
-if (s[i] == '-' || s[i] == '+')
-{
-if (s[i] == '-')
-sign = -1;
-i++;
-}
-while (s[i] >= '0' && s[i] <= '9')
-{
-num = num * 10 + (s[i] - '0');
-i++;
-}
-{return (num * sign); }
+    int i = 0, sign = 1, num = 0;
+    
+    while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
+           s[i] == '\v' || s[i] == '\f' || s[i] == '\r')
+        i++;
+    
+    if (s[i] == '-' || s[i] == '+')
+    {
+        if (s[i] == '-')
+            sign = -1;
+        i++;
+    }
+    
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        num = num * 10 + (s[i] - '0');
+        i++;
+    }
+    
+    return (num * sign);
 }
