130f97a71dd9fb6d77dbead07beae8290355c961
d-alshehri <10674@holbertonstudents.com>
2025-03-02 10:09:22 +0000

    T

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer to the standard output.
- * @n: The integer to be printed.
+ * print_number - prints an integer
+ * @n: the integer to print
  */
 void print_number(int n)
 {
@@ -11,9 +11,9 @@ if (n < 0)
 _putchar('-');
 n = -n;
 }
+
 if (n / 10)
-{
 print_number(n / 10);
-}
+
 _putchar((n % 10) + '0');
 }
