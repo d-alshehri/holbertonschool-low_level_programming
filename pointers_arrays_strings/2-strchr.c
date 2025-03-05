3fe92cbf42f645208fad1e26afcd0a84d3b3c992
d-alshehri <10674@holbertonstudents.com>
2025-03-05 09:53:53 +0000

    T

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/** 
+/**
 * _strchr - finds the first occurrence of a character in a string.
 *
 * This function searches for the first occurrence of the character `c`
 * in the string `s`. If found, it returns a pointer to that position.
 * If the character is not found, it returns NULL.
-* 
+*
 * @s: the input string
 * @c: the characer to find
 *
 * Return: A pointer to the first occurrence of `c`, or NULL if not found.
 */
 
-char *_strchr(char *s, char c) {
-int i;
+char *_strchr(char *s, char c)
+{int i;
 for (i = 0; s[i] != '\0'; i++)
-{if (*(s + i) == c)
+{
+if (*(s + i) == c)
 {return (s + i); }
 }
 if (c == '\0')
