e99173293d3f4802e5e6680670c75dddc609b73d
d-alshehri <10674@holbertonstudents.com>
2025-03-05 23:55:40 +0000

    T

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -13,11 +13,11 @@
  *
  * Return: This function does not return any value.
  */
- 
- void _puts_recursion(char *s){
-if (*s == '\0')
+
+void _puts_recursion(char *s)
+{if (*s == '\0')
 {_putchar('\n');
 {return; } }
 _putchar(*s);
-_puts_recursion(s+1);
+_puts_recursion(s + 1);
 }
