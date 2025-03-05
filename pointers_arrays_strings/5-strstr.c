dbbdad54791276112e828b4ace2529b4e00997e8
d-alshehri <10674@holbertonstudents.com>
2025-03-05 12:11:11 +0000

    T

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,12 +12,12 @@
  *   The function stops searching when a match is found, or when the end of
  *   `haystack` is reached.
  *
- * Return: 
+ * Return:
  *   - A pointer to the beginning of the located substring in `haystack`,
  *     or
  *   - NULL if the substring is not found.
  */
-  
+
 char *_strstr(char *haystack, char *needle)
 {
 int i = 0;
@@ -25,8 +25,8 @@ int j = 0;
 if (*needle == '\0')
 {return haystack; }
 while (haystack[i] != '\0')
-{if (haystack[i] == needle[0]){
-j = 0;
+{if (haystack[i] == needle[0])
+{j = 0;
 while (needle[j] != '\0' && haystack[i + j] == needle[j])
 {j++; }
 if (needle[j] == '\0')
@@ -34,5 +34,5 @@ if (needle[j] == '\0')
 }
 i++;
 }
-return NULL;
+{return NULL; }
 }
