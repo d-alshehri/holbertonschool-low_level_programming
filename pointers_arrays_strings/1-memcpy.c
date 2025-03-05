3f3c85b198d314a565ace3bb0d3786c65514ace7
d-alshehri <10674@holbertonstudents.com>
2025-03-05 01:24:47 +0000

    T

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -13,8 +13,8 @@
  * to the destination memory area after the copy is complete.
  */
 
-char *_memcpy(char *dest, char *src, unsigned int n) {
-unsigned int i;
+char *_memcpy(char *dest, char *src, unsigned int n)
+{unsigned int i;
 for (i = 0; i < n; i++)
 dest[i] = src[i];
 {return dest; }
