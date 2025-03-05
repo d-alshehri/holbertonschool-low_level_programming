2f8e5ad355099406efda5a00f286463e9a81a405
d-alshehri <10674@holbertonstudents.com>
2025-03-05 01:23:09 +0000

    T

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _memcpy - Copies memory area
+ * @dest: Pointer to the destination memory area
+ * @src: Pointer to the source memory area
+ * @n: The number of bytes to copy from src to dest
+ * Return: A pointer to the destination memory area (dest)
+ * Description: This function copies n bytes from memory area src to
+ * memory area dest. It handles raw memory copying, not null-terminated
+ * strings. The function copies data byte by byte and returns a pointer
+ * to the destination memory area after the copy is complete.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n) {
+int i;
+for (i = 0; i < n; i++)
+dest[i] = src[i];
+{return dest; }
+}
