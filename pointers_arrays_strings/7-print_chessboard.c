5456e1ee8d818e9dcc61202278d2f83259f4b51f
d-alshehri <10674@holbertonstudents.com>
2025-03-05 12:27:21 +0000

    T

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,24 +1,18 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_chessboard - Prints the chessboard
- * @a: A pointer to an 8x8 array representing the chessboard
- *
- * This function takes a pointer to an 8x8 2D array representing a chessboard.
- * It prints the chessboard in a grid format where each character represents
- * a chess piece or an empty space. After each row, a newline is printed to
- * maintain the structure of the chessboard.
+ * print_chessboard - prints the chessboard.
+ * @a: 2D array representing the chessboard
  */
-
 void print_chessboard(char (*a)[8])
 {
-for (int i = 0; i < 8; i++)
+int i, j;
+for (i = 0; i < 8; i++)
 {
-for (int j = 0; j < 8; j++)
+for (j = 0; j < 8; j++)
 {
-putchar(a[i][j]);
+_putchar(a[i][j]);
 }
-putchar('\n');
+_putchar('\n');
 }
 }
