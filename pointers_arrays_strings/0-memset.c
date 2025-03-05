46bbe3b6a2cd38667d0cd0650c41537e23ddfd85
d-alshehri <10674@holbertonstudents.com>
2025-03-05 01:04:56 +0000

    T

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *_memset(char *s, char b, unsigned int n){
-unsigned int i;
-for (i=0; i<n ; i++) {
+char *_memset(char *s, char b, unsigned int n)
+{unsigned int i;
+for (i = 0; i < n ; i++)
 *(s + i) = b;
-}
 {return s; }
 }
