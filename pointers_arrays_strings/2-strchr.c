19fd4e6a93e7ad47e4f3edced5312ee20778a983
d-alshehri <10674@holbertonstudents.com>
2025-03-05 09:49:54 +0000

    T

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -19,10 +19,12 @@
 
 char *_strchr(char *s, char c) {
 int i;
-for (i = 0; s[i] != '\0' ; i++) {
-if (*(s+i) == c) {
-return (s+i);
+for (i = 0; s[i] != '\0'; i++) {
+if (*(s+i) == c)
+{return (s+i); }
 }
-}
-{return NULL; }
+if (c == '\0')
+{return (s+i); }
+else
+{return (NULL); }
 }
