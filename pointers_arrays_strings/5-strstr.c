0eb5089ae1929e2749addf365a99a5e897ce72cc
d-alshehri <10674@holbertonstudents.com>
2025-03-05 12:09:45 +0000

    T

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -20,10 +20,13 @@
   
 char *_strstr(char *haystack, char *needle)
 {
-int i = 0;   
-while (haystack[i] != '\0'){
-int j = 0;        
-if (haystack[i] == needle[0]){
+int i = 0;
+int j = 0;
+if (*needle == '\0')
+{return haystack; }
+while (haystack[i] != '\0')
+{if (haystack[i] == needle[0]){
+j = 0;
 while (needle[j] != '\0' && haystack[i + j] == needle[j])
 {j++; }
 if (needle[j] == '\0')
