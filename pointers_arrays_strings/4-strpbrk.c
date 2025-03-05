ac9a46815bf64a2759acfb8cbc8f91941a237901
d-alshehri <10674@holbertonstudents.com>
2025-03-05 11:04:39 +0000

    T

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -10,17 +10,6 @@
  * Return: A pointer to the first occurrence of any byte from @accept in @s,
  *         or NULL if no such byte is found.
  *
- * Description:
- * This function iterates over the string @s and searches for the first character
- * that matches any character in the string @accept. The function returns a pointer
- * to the byte in @s that matches one of the bytes in @accept, or NULL if no match
- * is found after scanning the entire string.
- *
- * Example:
- *     char *s = "hello, world";
- *     char *accept = "wo";
- *     char *result = _strpbrk(s, accept);
- *     printf("%s\n", result);  // Output: "o, world"
  */
 
 char *_strpbrk(char *s, char *accept) {
