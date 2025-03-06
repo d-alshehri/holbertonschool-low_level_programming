a751ec8a63a7f7b5ab4176aa21e5865254ae3bf4
d-alshehri <10674@holbertonstudents.com>
2025-03-06 10:22:45 +0000

    T

--- recursion/main.h
+++ recursion/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int check_prime(int n, int i);
+int is_prime_number(int n);
 int _sqrt_helper(int n, int guess);
 int _sqrt_recursion(int n);
 int _pow_recursion(int x, int y);
