944fbc2ee1e6be8c2e058b10ff60355d2facd47d
d-alshehri <10674@holbertonstudents.com>
2025-03-06 00:53:11 +0000

    T

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -11,15 +11,15 @@
  * of the string until the null terminator is reached, and it counts the number
  * of characters before the null terminator.
  */
- 
-int _strlen_recursion(char *s){
-int count = 0;
+
+int _strlen_recursion(char *s)
+{int count = 0;
 if (*s == '\0')
 {return 0; }
 else
 {
-count ++;
-count += _strlen_recursion((s+1));
+count++;
+count + = _strlen_recursion((s+1));
 }
-return count;
+{return count; }
 }
