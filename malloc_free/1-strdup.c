f78e46d3031c69aa03f68cd1845e04ad73e4cb6f
d-alshehri <10674@holbertonstudents.com>
2025-03-09 11:38:34 +0000

    T

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -16,7 +16,7 @@ char *dup_str;
 if (str == NULL)
 {return NULL; }
 dup_str = malloc(strlen(str) + 1);
-if (dup_str == NULL) {
+if (dup_str == NULL)
 {return NULL; }
 strcpy(dup_str, str);
 {return dup_str; }
