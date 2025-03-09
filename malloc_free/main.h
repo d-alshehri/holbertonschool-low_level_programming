abd8e80bad5ce924f9a7a12c0155c1aafee3f305
d-alshehri <10674@holbertonstudents.com>
2025-03-09 12:30:57 +0000

    T

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -6,5 +6,7 @@
 char *str_concat(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
 
 #endif /* MAIN_H */
