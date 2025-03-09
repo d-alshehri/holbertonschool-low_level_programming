e609b3886f2cfbb8ac4d11e8f729aad8f29fb2b3
d-alshehri <10674@holbertonstudents.com>
2025-03-09 11:20:22 +0000

    T

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,11 +15,9 @@ unsigned int i;
 if (size == 0)
 {return NULL; }
 arr = malloc(size * sizeof(char));
-if (arr == NULL) {
+if (arr == NULL)
 {return NULL; }
-}
-for(i = 0; i < size; i++){
+for(i = 0; i < size; i++)
 arr[i] = c;
-}
 {return arr; }
 }
