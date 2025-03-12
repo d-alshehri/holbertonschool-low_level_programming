daaf88a2c97cad8d8aa049329177e9c402ddc3ff
d-alshehri <10674@holbertonstudents.com>
2025-03-12 11:53:10 +0000

    T

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a new dog structure
+ * @name: Pointer to dog's name
+ * @age: Dog's age
+ * @owner: Pointer to dog's owner name
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
