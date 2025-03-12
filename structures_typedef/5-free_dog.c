499579e903929e28ff7afc760ce254627e50f2c6
d-alshehri <10674@holbertonstudents.com>
2025-03-12 12:27:47 +0000

    T

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -7,13 +7,11 @@
  */
 void free_dog(dog_t *d)
 {
-    if (d == NULL)
-        return;
-
-    if (d->name)
-        free(d->name);
-    if (d->owner)
-        free(d->owner);
-
-    free(d);
+if (d == NULL)
+{return; }
+if (d->name)
+free(d->name);
+if (d->owner)
+free(d->owner);
+free(d);
 }
