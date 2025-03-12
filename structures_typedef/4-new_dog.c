fc5b2daa632f5b1096a43bba6b20c1403f29b00b
d-alshehri <10674@holbertonstudents.com>
2025-03-12 12:20:50 +0000

    T

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,6 +4,22 @@
 #include "dog.h"
 
 /**
+ * _strlen - manually calculates the length of a string
+ * @s: string whose length to calculate
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+int len = 0;
+while (s[len] != '\0')
+{
+len++;
+}
+{return len; }
+}
+
+/**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
@@ -13,33 +29,37 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *dog;
-    char *name_copy, *owner_copy;
+dog_t *dog;
+char *name_copy, *owner_copy;
 
-    dog = malloc(sizeof(dog_t));
-    if (dog == NULL)
-        return (NULL);
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+{return (NULL); }
 
-    name_copy = malloc(strlen(name) + 1);
-    if (name_copy == NULL)
-    {
-        free(dog);
-        return (NULL);
-    }
-    strcpy(name_copy, name);
+name_copy = malloc(_strlen(name) + 1);
+if (name_copy == NULL)
+{
+free(dog);
+return (NULL);
+}
+for (int i = 0; name[i] != '\0'; i++)
+name_copy[i] = name[i];
+name_copy[_strlen(name)] = '\0';
 
-    owner_copy = malloc(strlen(owner) + 1);
-    if (owner_copy == NULL)
-    {
-        free(name_copy);
-        free(dog);
-        return (NULL);
-    }
-    strcpy(owner_copy, owner);
+owner_copy = malloc(_strlen(owner) + 1);
+if (owner_copy == NULL)
+{
+free(name_copy);
+free(dog);
+{return (NULL); }
+}
+for (int i = 0; owner[i] != '\0'; i++)
+owner_copy[i] = owner[i];
+owner_copy[_strlen(owner)] = '\0';
 
-    dog->name = name_copy;
-    dog->age = age;
-    dog->owner = owner_copy;
+dog->name = name_copy;
+dog->age = age;
+dog->owner = owner_copy;
 
-    return (dog);
+{return (dog); }
 }
