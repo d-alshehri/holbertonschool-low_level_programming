5e804ee97abebcb81a2ae1a17131ef29beeee171
d-alshehri <10674@holbertonstudents.com>
2025-03-17 10:45:57 +0000

    function_pointers

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
+#include <string.h>
 
 int (*get_op_func(char *s))(int, int)
 {
@@ -13,8 +14,12 @@ int (*get_op_func(char *s))(int, int)
     };
     int i = 0;
 
-    while (ops[i].op != NULL && *(ops[i].op) != *s)
+    while (ops[i].op != NULL)
+    {
+        if (strcmp(ops[i].op, s) == 0)
+            return ops[i].f;
         i++;
+    }
 
-    return ops[i].f;
+    return NULL;
 }
