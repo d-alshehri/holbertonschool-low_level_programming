685de6e60c9eb240e6b74a2a1a6dbb92680414a8
d-alshehri <10674@holbertonstudents.com>
2025-03-18 11:00:11 +0000

    variadic_functions

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -9,19 +9,19 @@
  * Return: Sum of all parameters, or 0 if n is 0.
  */
 
-int sum_them_all(const unsigned int n, ...){
+int sum_them_all(const unsigned int n, ...)
+{
 int i = 0;
 int total = 0;
-   
+
 va_list elements;
-if ( n==0 )
+if (n == 0)
 {return 0; }
-   
+
 va_start(elements, n);
    
-for (i = 0; i < (int)n ; i++){
+for (i = 0; i < (int)n ; i++)
 total += va_arg(elements, int);
-}
 va_end(elements);
-{return total; }
+return total;
 }
