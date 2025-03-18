68ef90b0078878f63d399ec17c3849486e2f5e8d
d-alshehri <10674@holbertonstudents.com>
2025-03-18 12:13:25 +0000

    variadic_functions

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -8,38 +8,35 @@
  */
 void print_all(const char * const format, ...)
 {
-va_list args;
-unsigned int i = 0;
-char *str;
-char c;
-int num;
-float f;
+    va_list args;
+    unsigned int i = 0;
+    char *str;
 
-va_start(args, format);
+    va_start(args, format);
 
-while (format && format[i])
-{
-if (i > 0)
-printf(", ");
+    while (format && format[i])
+    {
+        if (i > 0)
+            printf(", ");
 
-if (format[i] == 'c')
-printf("%c", va_arg(args, int));  // 'char' is promoted to 'int'
-if (format[i] == 'i')
-printf("%d", va_arg(args, int));
-if (format[i] == 'f')
-printf("%f", va_arg(args, double));  // 'float' is promoted to 'double'
-if (format[i] == 's')
-{
-str = va_arg(args, char *);
-if (str)
-printf("%s", str);
-if (!str)
-printf("(nil)");
-}
+        if (format[i] == 'c')
+            printf("%c", va_arg(args, int));  /* 'char' is promoted to 'int' */
+        if (format[i] == 'i')
+            printf("%d", va_arg(args, int));
+        if (format[i] == 'f')
+            printf("%f", va_arg(args, double));  /* 'float' is promoted to 'double' */
+        if (format[i] == 's')
+        {
+            str = va_arg(args, char *);
+            if (str)
+                printf("%s", str);
+            if (!str)
+                printf("(nil)");
+        }
 
-i++;
-}
+        i++;
+    }
 
-printf("\n");
-va_end(args);
+    printf("\n");
+    va_end(args);
 }
