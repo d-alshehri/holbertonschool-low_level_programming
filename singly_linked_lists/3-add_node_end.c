faf5eda940b209e5f11893d61ebe71eaf295990b
d-alshehri <10674@holbertonstudents.com>
2025-04-01 13:22:24 +0000

    singly_linked_lists

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -3,6 +3,23 @@
 #include <string.h>
 
 /**
+ * _strlen - Custom implementation to calculate the length of a string.
+ * @str: The string to measure.
+ *
+ * Return: The length of the string.
+ */
+size_t _strlen(const char *str)
+{
+size_t len = 0;
+
+while (str[len] != '\0')
+{
+len++;
+}
+{return (len); }
+}
+
+/**
  * add_node_end - Adds a new node at the end of a list_t list.
  * @head: Double pointer to the head of the list.
  * @str: The string to be duplicated and added to the new node.
@@ -15,16 +32,16 @@ list_t *new_node, *temp;
 
 new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
-return (NULL);
-
+{return (NULL); }
 
 new_node->str = strdup(str);
 if (new_node->str == NULL)
 {
-free(new_node);  /* Free the node if strdup fails */
-return (NULL);
+free(new_node);
+{return (NULL); }
 }
-new_node->len = strlen(str);
+
+new_node->len = _strlen(str);
 new_node->next = NULL;
 
 if (*head == NULL)
@@ -33,7 +50,6 @@ if (*head == NULL)
 }
 else
 {
-
 temp = *head;
 while (temp->next != NULL)
 temp = temp->next;
