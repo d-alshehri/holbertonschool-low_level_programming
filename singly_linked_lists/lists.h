1471698ff1d23f6ab3ae5a9417820b69c81ebe1b
d-alshehri <10674@holbertonstudents.com>
2025-04-01 13:05:03 +0000

    singly_linked_lists

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,23 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+/**
+ * struct list_s - Singly linked list
+ * @str: String (malloc'ed string)
+ * @len: Length of the string
+ * @next: Points to the next node
+ *
+ * Description: Singly linked list node structure
+ */
+typedef struct list_s
+{
+char *str;
+unsigned int len;
+struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 
-#endif /* MAIN_H */
+#endif /* LISTS_H */
