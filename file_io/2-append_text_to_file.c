e1dda8bca5cd118867a122bd6198fc5040494978
d-alshehri <10674@holbertonstudents.com>
2025-04-03 06:03:04 +0000

    file_io

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -11,12 +11,12 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int fd,len=0, bytes_written;
+int fd, len = 0, bytes_written;
 if (filename == NULL)
-return( -1);
+return (-1);
 fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
-return( -1);
+return (-1);
 if (text_content)
 {
 while (text_content[len])
@@ -25,9 +25,9 @@ bytes_written = write(fd, text_content, len);
 if (bytes_written == -1)
 {
 close(fd);
-return( -1);
+return (-1);
 }
 }
 close(fd);
-return(1);
+return (1);
 }
