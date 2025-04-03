2b81e252e2597fbd5d4a2993b6e52eefb7f759ea
d-alshehri <10674@holbertonstudents.com>
2025-04-03 05:57:04 +0000

    file_io

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,18 +11,18 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-int fd, len=0, bytes_written;
-if (filename==NULL)
+int fd, len = 0, bytes_written;
+if (filename == NULL)
 {return(-1); }
-fd=open(filename, O_CREAT|O_WRONLY|O_TRUNC, 0600);
-if (fd == - 1)
+fd=open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (fd == -1)
 {return(-1); }
 if (text_content)
 {
 while (text_content[len])
 len++;
 bytes_written = write(fd, text_content, len);
-if (bytes_written == - 1)
+if (bytes_written == -1)
 {
 close(fd);
 {return(-1); }
