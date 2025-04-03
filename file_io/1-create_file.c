240db11ace80f3cbd463102c344c0bf21af6697c
d-alshehri <10674@holbertonstudents.com>
2025-04-03 05:57:36 +0000

    file_io

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 int fd, len = 0, bytes_written;
 if (filename == NULL)
 {return(-1); }
-fd=open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (fd == -1)
 {return(-1); }
 if (text_content)
